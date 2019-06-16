c6ea6ce1bd1337d2fc3a707a71733a0eb2d3c7dd
rraabe <49948103+rraabe@users.noreply.github.com>
2019-06-16 15:55:37 -0400

    Archived original file, new file named index.js. Finish foyer, inventory, and more spaces

--- index.js
+++ index.js
@@ -9,243 +9,153 @@ function ask(questionText) {
 }
 /*~~~~~~~~~~~~~~~~~~~~~~End Boiler Plate~~~~~~~~~~~~~~~~~~~~*/
 
-// remember the StateMachine lecture
-// https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
-let states = {
-  'roomOne': { 
-    canChangeTo: ['roomTwo'], 
-    name: "182 Main St.",
-    description: `You are standing on Main Street between Church and South Winooski.
-     There is a door here. A keypad sits on the handle.
-     On the door is a handwritten sign.`,
-    actions: ["read", "sign", "take", "open", "door", "enter", "code", "12345", "key"],
-    lockStatus: "locked",
-    unlock : function(){
-      this.lockStatus = "unlocked"
-      console.log("Success! The door opens. You enter the foyer and the door shuts behind you.");
-      playerObject.changeRoom(this.canChangeTo);
-    },
-    read: function(){
-      console.log(`\nThe sign says "Welcome to Burlington Code Academy! Come on 
-      up to the third floor. If the door is locked, use the code
-      12345."\n`);
-    },
-    take: function() {
-      console.log(`\nThat would be selfish. How will other students find their way?`);
-    },
-    inventory: null,
-    doorLocked: '\nThe door is locked. There is a keypad on the door handle.',
-    
-    ifChecks: function(arr) {
-      if(arr.includes("read") && arr.includes("sign")) {
-        this.read();
-        play();
-      } 
-      if(arr.includes("take") && arr.includes("sign")) {
-        this.take();
-        play();
-      } 
-      if(arr.includes("open") && arr.includes("door") && this.lockStatus === "locked") {
-        console.log(this.doorLocked);
-        play();
-      } 
-      if((arr.includes("enter") || arr.includes("key")) && arr.includes("code")){
-        if(arr.includes("12345")) {
-            this.unlock();
-            play();
-        }else{
-          console.log("Bzzzzt! The door is still locked.");
-          play();
-        }
-      }
-      else {
-        console.log(`Sorry, I don't know how to ${arr.join(" ")}.`);
-        play();
-      }
-    }  
-  },
-    'roomTwo': { canChangeTo: [ 'roomThree' ] },
-    'roomThree': { canChangeTo: [ 'roomOne' ] }
-};
-
-//Currently not using
-function enterState(newState) {
-  let validTransitions = states[currentState].canChangeTo;
-  if (validTransitions.includes(newState)) {
-    currentState = newState;
-  } else {
-    throw 'Invalid state transition attempted - from ' + currentState + ' to ' + newState;
+//Classes
+class Room {
+  constructor(name, description, inventory, canChangeTo, actions){
+  this.name = name;
+  this.description = description;
+  this.roomInventory = inventory;
+  this.canChangeTo = canChangeTo;
+  this.actions = actions;
+  //this.actions = actions;
   }
 }
 
-start();
-
-function start() {
-  console.log(`182 Main St.
-  You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`);
-play();
-    // process.exit();
+class Item{
+  constructor(name, description, canPickUp){
+    this.name = name;
+    this.description = description;
+    this.canPickUp = canPickUp;
+  }
 }
 
-async function play() {
-  let answer = await ask("What would you like to do?\n>_");
-  let answerArr = answer.split(" ");
-  states[playerObject.currentState].ifChecks(answerArr); //This function will need to be more general and could take the answerArr and the currentState
-  
-}
+//Rooms
+let roomOne = new Room(
+  "182 Main St.",
+  `You are standing on Main Street between Church and South Winooski.
+  There is a door here. A keypad sits on the handle.
+  On the door is a handwritten sign.`,
+  {'read sign': `The sign says "Welcome to Burlington Code Academy! Come on 
+  up to the third floor. If the door is locked, use the code
+  12345."`},
+  ['roomTwo'],
+  {12345: 'roomTwo', 'take sign': "That would be selfish, how would other students find their way?", 'open door': `The door is locked. There is a keypad on the door handle.`} 
+);
 
-// let roomOne = {
-//   name: "182 Main St.",
-//   description: `You are standing on Main Street between Church and South Winooski.
-//   There is a door here. A keypad sits on the handle.
-//   On the door is a handwritten sign.`,
-//   actions: ["read", "sign", "take", "open", "door", "enter", "code", "12345", "key"],
-//   canChangeTo: ['roomTwo'],
-//   lockStatus: "locked",
-//   unlock : function(){
-//     this.lockStatus = "unlocked"
-//     console.log("Success! The door opens. You enter the foyer and the door shuts behind you.");
-//     playerObject.changeRoom(this.canChangeTo);
-//   },
-//   read: function(){
-//     console.log(`\nThe sign says "Welcome to Burlington Code Academy! Come on 
-//     up to the third floor. If the door is locked, use the code
-//     12345."`);
-//   },
-//   take: function() {
-//     console.log(`\nThat would be selfish. How will other students find their way?`);
-//   },
-//   inventory: null,
-//   doorLocked: '\nThe door is locked. There is a keypad on the door handle.',
-  
-//   roomOneChecks: function(arr) {
-//     if(arr.includes("read") && arr.includes("sign")) {
-//       playerObject.currentState.read();
-//       play();
-//     } 
-//     if(arr.includes("take") && arr.includes("sign")) {
-//       playerObject.currentState.take();
-//       play();
-//     } 
-//     if(arr.includes("open") && arr.includes("door") && playerObject.currentState.lockStatus === "locked") {
-//       console.log(playerObject.currentState.doorLocked);
-//       play();
-//     } 
-//     if((arr.includes("enter") || arr.includes("key")) && arr.includes("code")){
-//       if(arr.includes("12345")) {
-//           playerObject.currentState.unlock();
-//           play();
-//       }else{
-//         console.log("Bzzzzt! The door is still locked.");
-//         play();
-//       }
-//     }
-//     else {
-//       console.log(`Sorry, I don't know how to ${arr.join(" ")}.`);
-//       play();
-//     }
-//   }  
-// }
-
-let roomTwo = {
-  name: "182 Main St. - Foyer",
-  description: `You are in a foyer. Or maybe it's an antechamber. Or a 
+let roomTwo = new Room(
+  '182 Main St. - Foyer',
+  `You are in a foyer. Or maybe it's an antechamber. Or a 
   vestibule. Or an entryway. Or an atrium. Or a narthex.
   But let's forget all that fancy flatlander vocabulary,
   and just call it a foyer. In Vermont, this is pronounced
-  "FO-ee-yurr".`,
-  actions: [],
-  canChangeTo: ['roomOne', 'roomThree'],
-  lockStatus: "unlocked",
-  unlock : function(){
-    playerObject.changeRoom(this.canChangeTo);
-  },
-  read: function(){
-    console.log(`\nText for Seven Days`);
-  },
-  take: function() {
-    console.log(`\nYou pick up the Seven Days`);
-    //function that removes the Seven Days from the room inventory (set value to null?) and creates it in the player inventory
-  },
-  inventory: {
-    "Seven Days": `A copy of Seven Days lies in a corner.` //Object.key(roomTwo.inventory).forEach=>(key){console.log(key)}
-  },
-  doorLocked: '\nThe door is locked. There is a keypad on the door handle.',
+  "FO-ee-yurr".
+  A copy of Seven Days lies in a corner.`,
+  {'seven days': `You pick up the paper and leaf through it looking for comics 
+  and ignoring the articles, just like everybody else does.`}, //inventory
+  ['roomOne', 'roomThree'],
+  {} //actions
+  );
+
+  //Do we need a lookup table for [current].description to work? Marshall mentioned that you can't have the first 
+  //key be a variable unless you use a lookup table to convert the string to an object by naming itself
+  const roomLookupTable = {
+    roomOne: roomOne,
+    roomTwo: roomTwo,
+    //roomThree: roomThree
+  }
+
   
-  roomTwoChecks: function(arr) {
-    if(arr.includes("read") && arr.includes("sign")) {
-      playerObject.currentState.read();
-      play();
-    } 
-    if(arr.includes("take") && arr.includes("sign")) {
-      playerObject.currentState.take();
-      play();
-    } 
-    if(arr.includes("open") && arr.includes("door") && playerObject.currentState.lockStatus === "locked") {
-      console.log(playerObject.currentState.doorLocked);
-      play();
-    } 
-    if((arr.includes("enter") || arr.includes("key")) && arr.includes("code")){
-      if(arr.includes("12345")) {
-          playerObject.currentState.unlock();
-          play();
-      }else{
-        console.log("Bzzzzt! The door is still locked.");
-        play();
+  //player
+  let player = {
+    currentState: 'roomOne',
+    playerInventory: [],
+  //Will need to add functions for interacting - pickUp, drop, look, read, etc...
+  actions: {
+    look(){
+      console.log([currentState].description);
+    },
+    read(action, itemName){
+      //console.log(`The value of passed argument in the read function was: ${itemName}`);
+      if(Object.keys(roomLookupTable[player.currentState].roomInventory).includes(action + " " + itemName)){
+        console.log(roomLookupTable[player.currentState].roomInventory[action + " " + itemName]);
+        getInput();
+      } else {
+        console.log(`I'm sorry I can't read the ${itemName}`);
+        getInput();
       }
+    },
+    enter(action, code) {
+      if(code === '12345'){
+        console.log('Success! The door opens. You enter the foyer and the door shuts behind you.')
+        enterState(roomLookupTable[player.currentState].actions[code]);
+      } else {
+        console.log(`Bzzzzt! The door is still locked.`);
+        getInput();
+      }
+    },
+    take(action, itemName){ 
+      if(Object.keys(roomLookupTable[player.currentState].actions).includes(action + " " + itemName)){
+      console.log(roomLookupTable[player.currentState].actions[action + " " + itemName]);
+      getInput();
+      } else {
+      console.log(`I'm sorry I can't take the ${itemName}`);
+      getInput();
+     }
+    }, 
+
+    open(action, itemName) {
+      if(Object.keys(roomLookupTable[player.currentState].actions).includes(action + " " + itemName)){
+        console.log(roomLookupTable[player.currentState].actions[action + " " + itemName]);
+        getInput();
+        } else {
+        console.log(`I'm sorry I can't open the ${itemName}`);
+        getInput();
+       }
     }
-    else {
-      console.log(`Sorry, I don't know how to ${arr.join(" ")}.`);
-      play();
-    }
-  }  
-}
-
-// let states = {
-//   "green": {canChangeTo: ["yellow"]},
-//   "yellow": {canChangeTo: ["red"]},
-//   "red": {canChangeTo: ["green"]}
-// }
-
-// let currentState = "green";
-
-// function enterState(newState) {
-//   let validTransitions = states[currentState].canChangeTo;
-//   if (validTransitions.includes(newState)) {
-//     currentState = newState;
-//   } else {
-//     throw "Invalid state transition attempted - from " + currentState + " to " + newState;
-//   }
-// }
-
-
-let playerObject = {
-  inventory: [],
-  currentState: 'roomOne',
-  changeRoom: function(newState){
-    let validTransitions = states[this.currentState].canChangeTo;
-    console.log("validTransitions = " + validTransitions)
-    if (validTransitions.includes(newState)) {
-          this.currentState = newState;
-    } else {
-        throw "Invalid state transition attempted - from " + [this.currentState] + " to " + newState;
-    } 
   }
 }
 
 
-//Saving for later
-// class Room {
-//   constructor(name, description, actions, canChangeTo, lockStatus){
-//   this.name = name;
-//   this.description = description;
-//   this.actions = actions;
-//   this.canChangeTo = canChangeTo;
-//   this.lockStatus = lockStatus;
-// }
-//let roomOne = new Room()
+
+function enterState(newState) {
+  //console.log(`enterState with ${newState} as the argument`)
+  let validTransitions = roomLookupTable[player.currentState].canChangeTo;
+  if (validTransitions.includes(newState)) {
+    player.currentState = newState;
+    newRoom();
+  } else {
+    throw 'Invalid state transition attempted - from ' + player.currentState + ' to ' + newState;
+  }
+}
+
+function start() {
+  console.log("Please enter your commands as two words. For example 'open door' or 'read sign'.");
+  //console.log(`currentState is: ${player.currentState}.`);
+  newRoom();
+}
+
+function newRoom(){
+  console.log(`${roomLookupTable[player.currentState].description}`);
+  getInput();
+}
+
+async function getInput() {
+  let input = await ask("What would you like to do?\n>_");
+  let arrInput = input.toLowerCase().split(" ");
+  checkInput(arrInput[0], arrInput[arrInput.length-1], arrInput);
+}
+
+
+function checkInput(arg1, arg2, arrInput){
+  if(Object.keys(player.actions).includes(arg1)){
+    //console.log(`The player action array does include: ${arg1}`);
+    player.actions[arg1](arg1, arg2);
+  }else{
+    console.log(`I'm sorry I don't know how to ${arrInput}`);
+    getInput();
+  }
+}
 
 
 
+start();
